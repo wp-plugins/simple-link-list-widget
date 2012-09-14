e163ad58dba70c2dbbbe21ac4b3fa5d11dc5a4d0
jimmywb <jimmywb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-14 20:18:49 +0000

    New Version
    git-svn-id: https://plugins.svn.wordpress.org/simple-link-list-widget/trunk@599076 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simple-link-list-widget.php
+++ simple-link-list-widget.php
@@ -3,8 +3,8 @@
 Plugin Name: Simple Link List Widget
 Description: Enables a link list widget, in which you can display items in an ordered or unordered list.  Based on Frankie Roberto's List Widget
 Author: James Bowles
-Author URI: http://mesacc.edu/~chago
-Version: 0.1
+Author URI: http://mesacc.edu/~jamul76751
+Version: 0.2
 */
 
 class SimpleLinkListWidget extends WP_Widget {
@@ -23,7 +23,8 @@ class SimpleLinkListWidget extends WP_Widget {
 		for ($i = 1; $i <= $amount; $i++) {
 			$items[$i] = $instance['item' . $i];
 			$item_links[$i] = $instance['item_link' . $i];
-			$item_classes[$i] = $instance['item_class' . $i];
+			$item_classes[$i] = $instance['item_class' . $i];	
+			$item_targets[$i] = isset($instance['item_target' . $i]) ? $instance['item_target' . $i] : false;
 		}
 
 		echo $before_widget .  $before_title . $title . $after_title;  ?>
@@ -34,7 +35,11 @@ class SimpleLinkListWidget extends WP_Widget {
 						if (empty($item_links[$num])) :
 							echo("<li class='" . $item_classes[$num] . "'>" . $item . "</li>");
 						else :
-							echo("<li class='" . $item_classes[$num] . "'><a href='" . $item_links[$num] . "'>" . $item . "</a></li>");
+							if($item_targets[$num]) :
+								echo("<li class='" . $item_classes[$num] . "'><a href='" . $item_links[$num] . "' target='_blank'>" . $item . "</a></li>");
+							else :
+								echo("<li class='" . $item_classes[$num] . "'><a href='" . $item_links[$num] . "'>" . $item . "</a></li>");
+							endif;
 						endif;
 					endif;
 				 endforeach; ?>
@@ -52,12 +57,15 @@ class SimpleLinkListWidget extends WP_Widget {
 		}else{
 			$instance['amount'] = $new_instance['new_amount'];
 		}
-
+		$j=0; //index to skip over blank items
 		for ($i = 1; $i <= $instance['amount']; $i++) {
-			$instance['item' . $i] = strip_tags($new_instance['item' . $i]);
-			$instance['item_link' . $i] = $new_instance['item_link' . $i];
-			$instance['item_class' . $i] = strip_tags($new_instance['item_class' . $i]);
-		}		
+			if(empty($new_instance['item' . $i])){ $j++; }
+			$instance['item' . $i] = strip_tags($new_instance['item' . ($j+$i)]);
+			$instance['item_link' . $i] = $new_instance['item_link' . ($j+$i)];
+			$instance['item_class' . $i] = strip_tags($new_instance['item_class' . ($j+$i)]);
+			$instance['item_target' . $i] = $new_instance['item_target' . ($j+$i)];
+		}
+		$instance['amount'] = $instance['amount'] - $j;	
 		$instance['type'] = $new_instance['type'];
 		
 		return $instance;
@@ -72,6 +80,7 @@ class SimpleLinkListWidget extends WP_Widget {
 			$items[$i] = $instance['item' . $i];
 			$item_links[$i] = $instance['item_link' . $i];
 			$item_classes[$i] = $instance['item_class' . $i];
+			$item_targets[$i] = $instance['item_target' . $i];
 		}
 		$title_link = $instance['title_link'];		
 		$type = empty($instance['type']) ? 'unordered' : $instance['type'] ;
@@ -79,7 +88,7 @@ class SimpleLinkListWidget extends WP_Widget {
 ?>
 		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
-		<p><small>Leave the Link and Custom Style Class fields blank if not desired. Click Save for additional blank fields.</small></p>
+		<p><small>Leave the Link and Custom Style Class fields blank if not desired. Click Save for additional blank fields. To remove an item simply delete the "Text:" field content.</small></p>
 		<ol>
 		<?php foreach ($items as $num => $item) : ?>
 		
@@ -90,6 +99,7 @@ class SimpleLinkListWidget extends WP_Widget {
 				<input class="widefat" id="<?php echo $this->get_field_id('item_link' . $num); ?>" name="<?php echo $this->get_field_name('item_link' . $num); ?>" type="text" value="<?php echo esc_attr($item_links[$num]); ?>" />
 				<label for="<?php echo $this->get_field_id('item_class' . $num); ?>">Custom Style Class:</label>
 				<input class="widefat" id="<?php echo $this->get_field_id('item_class' . $num); ?>" name="<?php echo $this->get_field_name('item_class' . $num); ?>" type="text" value="<?php echo esc_attr($item_classes[$num]); ?>" />
+				<label for="<?php echo $this->get_field_id('item_target' . $num); ?>"><input type="checkbox" name="<?php echo $this->get_field_name('item_target' . $num); ?>" id="<?php echo $this->get_field_id('item_target' . $num); ?>" <?php checked($item_targets[$num], 'on'); ?> /> Open in new window</label>
 			</li>
 		<?php endforeach; ?>
 		
@@ -102,13 +112,14 @@ class SimpleLinkListWidget extends WP_Widget {
 				<input class="widefat" id="<?php echo $this->get_field_id('item_link' . $new_amount); ?>" name="<?php echo $this->get_field_name('item_link' . $new_amount); ?>" type="text" value="" />
 				<label for="<?php echo $this->get_field_id('item_class' . $new_amount); ?>">Custom Style Class:</label>
 				<input class="widefat" id="<?php echo $this->get_field_id('item_class' . $new_amount); ?>" name="<?php echo $this->get_field_name('item_class' . $new_amount); ?>" type="text" value="" />
+                <label for="<?php echo $this->get_field_id('item_target' . $new_amount); ?>"><input type="checkbox" name="<?php echo $this->get_field_name('item_target' . $new_amount); ?>" id="<?php echo $this->get_field_id('item_target' . $new_amount); ?>" />  Open in new window</label>
 			</li>
 		</ol>
 		<input type="hidden" id="<?php echo $this->get_field_id('amount'); ?>" name="<?php echo $this->get_field_name('amount'); ?>" value="<?php echo $amount ?>" />
 		<input type="hidden" id="<?php echo $this->get_field_id('new_amount'); ?>" name="<?php echo $this->get_field_name('new_amount'); ?>" value="<?php echo $new_amount ?>" />
 
-		<label for="<?php echo $this->get_field_id('ordered'); ?>"><input type="radio" name="<?php echo $this->get_field_name('type'); ?>" value="ordered" id="<?php echo $this->get_field_id('ordered'); ?>" <?php checked($type, "ordered"); ?>></input>  Ordered</label>
-		<label for="<?php echo $this->get_field_id('unordered'); ?>"><input type="radio" name="<?php echo $this->get_field_name('type'); ?>" value="unordered" id="<?php echo $this->get_field_id('unordered'); ?>" <?php checked($type, "unordered"); ?>></input> Unordered</label>
+		<label for="<?php echo $this->get_field_id('ordered'); ?>"><input type="radio" name="<?php echo $this->get_field_name('type'); ?>" value="ordered" id="<?php echo $this->get_field_id('ordered'); ?>" <?php checked($type, "ordered"); ?> />  Ordered</label>
+		<label for="<?php echo $this->get_field_id('unordered'); ?>"><input type="radio" name="<?php echo $this->get_field_name('type'); ?>" value="unordered" id="<?php echo $this->get_field_id('unordered'); ?>" <?php checked($type, "unordered"); ?> /> Unordered</label>
 
 <?php
 	}
